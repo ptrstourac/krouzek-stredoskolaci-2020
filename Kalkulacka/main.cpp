3a9c6057f31e70d6f370ca6baeaf63d3f7e3c9ba
ptrstourac <ptrstourac@users.noreply.github.com>
2020-12-01 19:50:12 +0100

    Upgrade kalkulacka SW

--- Kalkulacka/main.cpp
+++ Kalkulacka/main.cpp
@@ -9,43 +9,54 @@ char operace;
 
 int vypocet(char operace, int hodnota1, int hodnota2)
 {
-    int output;
-    switch(operace)
+    int output = 0;
+    if (isdigit(hodnota1) && isdigit(hodnota2))
     {
-        case '+':
-            output = hodnota1 + hodnota2;
-            break;
+        switch(operace)
+        {
+            case '+':
+                output = hodnota1 + hodnota2;
+                break;
 
-        case '-':
-            output = hodnota1 - hodnota2;
-            break;
+            case '-':
+                output = hodnota1 - hodnota2;
+                break;
 
-        case '*':
-            output = hodnota1 * hodnota2;
-            break;
+            case '*':
+                output = hodnota1 * hodnota2;
+                break;
 
-        case '/':
-            output = hodnota1 / hodnota2;
-            break;
+            case '/':
+                if(hodnota2 != 0) {
+                   output = hodnota1 / hodnota2;
+                }
+                cout << "Nulou nelze delit! \n";
+                break;
 
-        default:
-            break;
+            default:
+                cout << "Nepis sem blbosti! \n";
+                return -2147483648;
+                break;
+        }
     }
     return output;
 }
 
 int main()
 {
-    cout << "Zadej operaci \n";
-    cin >> operace;
+    while(true){
+        cout << "Zadej operaci \n";
+        cin >> operace;
 
-    cout << "Napis cislo 1 \n";
-    cin >> cislo1;
+        cout << "Napis cislo 1 \n";
+        cin >> cislo1;
 
-    cout << "Napis cislo 2 \n";
-    cin >> cislo2;
+        cout << "Napis cislo 2 \n";
+        cin >> cislo2;
 
-    vysledek = vypocet(operace, cislo1, cislo2);
-    cout << "Vysledek: " << vysledek << "\n";
-    return 0;
+        vysledek = vypocet(operace, cislo1, cislo2);
+        if (vysledek != -2147483648){
+            cout << "Vysledek: " << vysledek << "\n";
+        } return 1;
+    }
 }
